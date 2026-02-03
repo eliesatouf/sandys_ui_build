9742f45c6edaa1270fd21862453fe1abef3cbe96
Elie <elie.satouf@gmail.com>
2026-02-03 09:09:08 +0800

    fix useFetch error

---
+++ _app/immutable/entry/start.CtzgQKn3.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/C1oQyOL_.js";import{x as t}from"../chunks/ZSZZDgiA.js";export{t as load_css,r as start};
---
+++ menuBanner.svelte
@@ -0,0 +1,226 @@
+<div class="flex flex-row ">
+<img 
+       src="/menus/01.jpg"
+       alt="Classic background"
+       class=" absolute top-0 left-0
+              w-70 h-70 object-cover
+              rounded-sm           
+              transform-gpu        
+              origin-bottom
+              rotate-5
+              z-1
+              brightness-110
+              inset-5
+              shadow-lg shadow-stone-600">
+<img 
+       src="/menus/02.jpg"
+       alt="Classic background"
+       class="absolute top-40 left-0
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           -rotate-5
+           z-2
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<!-- <img 
+       src="/menus/04.jpg"
+       alt="Classic background"
+       class=" absolute top-0 left-50 lg:left-90
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           -rotate-10
+           brightness-110
+           opacity-80
+           inset-5
+           z-1
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/05.jpg"
+       alt="Classic background"
+       class="absolute top-40 left-50 lg:left-90
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           rotate-10
+           z-1
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/06.jpg"
+       alt="Classic background"
+       class=" absolute top-40 left-10
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           rotate-10
+           z-1
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/09.jpg"
+       alt="Classic background"
+       class=" absolute top-70 left-150
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           -rotate-10
+           z-1
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/48.jpg"
+       alt="Classic background"
+       class=" absolute top-70 left-200
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           rotate-5
+           z-1
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/11.jpg"
+       alt="Classic background"
+       class="hidden sm:block absolute top-70 left-240
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           rotate-12
+           z-1
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/12.jpg"
+       alt="Classic background"
+       class="hidden sm:block absolute -top-30 left-240
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           rotate-12
+           z-1
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/13.jpg"
+       alt="Classic background"
+       class="hidden lg:block absolute -top-20 left-170
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           -rotate-12
+           z-1
+           inset-5
+           shadow-lg shadow-stone-600">
+
+           
+
+<img 
+       src="/menus/14.jpg"
+       alt="Classic background"
+       class="hidden lg:block absolute top-0 left-300
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           -rotate-12
+           z-1
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/34.jpg"
+       alt="Classic background"
+       class="hidden lg:block absolute top-0 left-350
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           -rotate-5
+           z-1
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/16.jpg"
+       alt="Classic background"
+       class="hidden lg:block absolute top-50 left-320
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           rotate-12
+           z-1
+           brightness-110
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/35.jpg"
+       alt="Classic background"
+       class="hidden lg:block absolute top-50 left-360
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           rotate-2
+           z-1
+           brightness-120
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/36.jpg"
+       alt="Classic background"
+       class="hidden lg:block absolute top-50 left-400
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           rotate-6
+           z-1
+           brightness-100
+           inset-5
+           shadow-lg shadow-stone-600">
+
+<img 
+       src="/menus/216.jpg"
+       alt="Classic background"
+       class="hidden lg:block absolute top-0 left-400
+           w-70 h-70 object-cover
+           rounded-sm           
+           transform-gpu        
+           origin-bottom
+           -rotate-6
+           z-1
+           brightness-100
+           inset-5
+           shadow-lg shadow-stone-600"> -->
+
+</div>
