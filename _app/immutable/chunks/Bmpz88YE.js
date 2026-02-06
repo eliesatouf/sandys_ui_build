26ce2e2da5871c748f71b73726efe0b0d8399880
Elie <elie.satouf@gmail.com>
2026-02-06 13:25:34 +0800

    fix width of variant section

---
+++ _app/immutable/chunks/Bmpz88YE.js
@@ -0,0 +1,1 @@
+import{p as r}from"./B2f-H9CZ.js";import{s as t}from"./OPsMoGeG.js";const e={get error(){return r.error},get status(){return r.status},get url(){return r.url}};t.updated.check;const a=e;export{a as p};
---
+++ _app/immutable/entry/start.DsJZ1AkF.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/OPsMoGeG.js";import{x as t}from"../chunks/B2f-H9CZ.js";export{t as load_css,r as start};
