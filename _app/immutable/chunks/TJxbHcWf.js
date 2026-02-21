49c1bf16c88db1c71e552a64dcde2251384a1046
Elie <elie.satouf@gmail.com>
2026-02-21 14:54:40 +0800

    clean up

--- _app/immutable/chunks/TJxbHcWf.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_rpu5zu?.base??"",a=globalThis.__sveltekit_rpu5zu?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.DB2xRf84.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DYemYBhR.js";import{w as t}from"../chunks/DvMRcHGc.js";export{t as load_css,r as start};
