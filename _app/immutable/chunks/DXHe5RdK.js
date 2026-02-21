e58108eaa4175baaeefb33bd737d30770f704d98
Elie <elie.satouf@gmail.com>
2026-02-21 20:02:56 +0800

    clean up

--- _app/immutable/chunks/DXHe5RdK.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_z4jryu?.base??"",a=globalThis.__sveltekit_z4jryu?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.DXuoRPGs.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DtsEzB2K.js";import{w as t}from"../chunks/BX8XBLTZ.js";export{t as load_css,r as start};
