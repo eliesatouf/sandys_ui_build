bf9e576a2b0cc6bc0fe26f0ad03091879c41db73
Elie <elie.satouf@gmail.com>
2026-02-22 19:09:22 +0800

    fix hero video to render only on desktop

---
+++ _app/immutable/chunks/ByQBzmBP.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1qidnbm?.base??"",a=globalThis.__sveltekit_1qidnbm?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.CV9OHlHx.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/B1GRjNwL.js";import{w as t}from"../chunks/D_FXFuV3.js";export{t as load_css,r as start};
