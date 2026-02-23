78c2386ab9592e6714d613ffc86d56bfbd458d3e
Elie <elie.satouf@gmail.com>
2026-02-23 21:20:23 +0800

    ..

---
+++ _app/immutable/chunks/BRPY45Um.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_nuymnk?.base??"",a=globalThis.__sveltekit_nuymnk?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.BFkErese.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BcOIEwVT.js";import{w as t}from"../chunks/Bulz3UMo.js";export{t as load_css,r as start};
