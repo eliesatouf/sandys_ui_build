a44048d27179bfe7c6c4adc13ae90abf021ee052
Elie <elie.satouf@gmail.com>
2026-02-23 21:43:57 +0800

    replaced event listeners with svelte navigation functions

---
+++ _app/immutable/chunks/0J4XhyVW.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_vy3ikc?.base??"",a=globalThis.__sveltekit_vy3ikc?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.GXwPNpBT.js
@@ -0,0 +1,1 @@
+import{c as s}from"../chunks/C6sROc1W.js";import{w as t}from"../chunks/tFKtnQSk.js";export{t as load_css,s as start};
