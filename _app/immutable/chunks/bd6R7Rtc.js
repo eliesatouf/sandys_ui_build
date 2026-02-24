e1462beb979451b82da25a9085997e176c2e0674
Elie <elie.satouf@gmail.com>
2026-02-24 15:31:42 +0800

    add landing page provider

---
+++ _app/immutable/chunks/bd6R7Rtc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_cadvkr?.base??"",a=globalThis.__sveltekit_cadvkr?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.E8HOJaON.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CK4VBx-F.js";import{w as t}from"../chunks/Dyl33dyz.js";export{t as load_css,r as start};
