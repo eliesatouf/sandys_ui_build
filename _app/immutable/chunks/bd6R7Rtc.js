1ccf742699f4a1d381afd9221fa3c7a943f26a92
Elie <elie.satouf@gmail.com>
2026-02-24 15:58:16 +0800

    clean up

--- _app/immutable/chunks/bd6R7Rtc.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_cadvkr?.base??"",a=globalThis.__sveltekit_cadvkr?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.E8HOJaON.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CK4VBx-F.js";import{w as t}from"../chunks/Dyl33dyz.js";export{t as load_css,r as start};
