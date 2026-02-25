07c5ab9ebf51129ab22796cdfebf65cfea4c70ce
Elie <elie.satouf@gmail.com>
2026-02-25 16:17:23 +0800

    change variant button style

---
+++ _app/immutable/chunks/CItSPS7Q.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_gips58?.base??"",a=globalThis.__sveltekit_gips58?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.C65wa9HA.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Bmm0lpN_.js";import{w as t}from"../chunks/BLztqHNJ.js";export{t as load_css,r as start};
