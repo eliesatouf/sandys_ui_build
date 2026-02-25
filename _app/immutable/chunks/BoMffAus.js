0871941fae2ba7dd3bb13b1c696aa3207d535721
Elie <elie.satouf@gmail.com>
2026-02-25 11:41:02 +0800

    removed splash temporary

---
+++ _app/immutable/chunks/BoMffAus.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1btk9p?.base??"",t=globalThis.__sveltekit_1btk9p?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/entry/start.Cnzr69Nb.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CXoCtAJO.js";import{w as t}from"../chunks/Dcc3-l4z.js";export{t as load_css,r as start};
