9322c925b1a15d710207863a2920c6ca2818935d
Elie <elie.satouf@gmail.com>
2026-02-25 12:46:13 +0800

    optimizing +page.js

---
+++ _app/immutable/chunks/BkM4b__R.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1lk0eri?.base??"",e=globalThis.__sveltekit_1lk0eri?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/entry/start.DhXyIyN8.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/mXKdeddv.js";import{w as t}from"../chunks/NCjyoDQA.js";export{t as load_css,r as start};
