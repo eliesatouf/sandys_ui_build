82a7dfbd73b09e628c4364fcc82a223e1229fe00
Elie <elie.satouf@gmail.com>
2026-02-25 17:51:29 +0800

    removed analytics script to troublshoot speed

---
+++ _app/immutable/chunks/H9bYjpmc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_2ymjef?.base??"",e=globalThis.__sveltekit_2ymjef?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/entry/start.CBv_TBk-.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DO15mNN-.js";import{w as t}from"../chunks/CW54NBX9.js";export{t as load_css,r as start};
