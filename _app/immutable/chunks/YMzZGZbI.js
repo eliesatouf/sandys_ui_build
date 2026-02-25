af79e717c9c9be55103b334811796164309cd82a
Elie <elie.satouf@gmail.com>
2026-02-25 17:59:46 +0800

    added analytics as component

---
+++ _app/immutable/chunks/YMzZGZbI.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_vpulsp?.base??"",a=globalThis.__sveltekit_vpulsp?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.D8UVvI4i.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Dqz1LPXK.js";import{w as t}from"../chunks/Q2FLgsbE.js";export{t as load_css,r as start};
