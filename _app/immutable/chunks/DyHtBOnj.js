9681d0fbfeaeb58070e56146386d27020411bf4b
Elie <elie.satouf@gmail.com>
2026-03-07 09:18:30 +0800

    fix dish in menu

---
+++ _app/immutable/chunks/DyHtBOnj.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_13pr4xk?.base??"",a=globalThis.__sveltekit_13pr4xk?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.BLR5TwiP.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CemghQBK.js";import{w as t}from"../chunks/CKiKDwu8.js";export{t as load_css,r as start};
