a94c1e869d9ca80d2009149a047b446639bc286f
Elie <elie.satouf@gmail.com>
2026-03-26 10:11:22 +0800

    testing pulling google reviews

---
+++ _app/immutable/chunks/4UeiCBMj.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_8ogahi?.base??"",a=globalThis.__sveltekit_8ogahi?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.BJt_zUGU.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/C7Qrc-uh.js";import{w as t}from"../chunks/CxrBenes.js";export{t as load_css,r as start};
