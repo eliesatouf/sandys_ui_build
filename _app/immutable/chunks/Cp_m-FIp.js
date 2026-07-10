f01c551e4d1d5d7533b8207b20c80959e9cc09a2
Elie <elie.satouf@gmail.com>
2026-07-11 07:32:37 +0800

    removed delivery option, fixed tag style

---
+++ _app/immutable/chunks/Cp_m-FIp.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_16740t2?.base??"",t=globalThis.__sveltekit_16740t2?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/entry/start.CvbOvCKf.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Fmn12l1R.js";import{w as t}from"../chunks/xjgRGCRs.js";export{t as load_css,r as start};
