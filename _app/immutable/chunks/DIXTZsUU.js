81ce780ce00b4591ea18199c801e2b06c7dbfaf5
Elie <elie.satouf@gmail.com>
2026-07-12 07:09:59 +0800

    fix menu behaviour

---
+++ _app/immutable/chunks/DIXTZsUU.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_vjy9h0?.base??"",a=globalThis.__sveltekit_vjy9h0?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.CTzveUl6.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/D6Nw8s9y.js";import{w as t}from"../chunks/CEJKwWY9.js";export{t as load_css,r as start};
