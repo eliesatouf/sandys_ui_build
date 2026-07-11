9e14539ee89b53d901f0258db22d0204dd55e7fa
Elie <elie.satouf@gmail.com>
2026-07-11 18:38:08 +0800

    fix 'Cannot read properties of null (reading 'length')' in /menu

---
+++ _app/immutable/chunks/CNAhO58I.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_74qel4?.base??"",e=globalThis.__sveltekit_74qel4?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/entry/start.CAMiTx9b.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DvvmW0-T.js";import{w as t}from"../chunks/ewH4aQyq.js";export{t as load_css,r as start};
